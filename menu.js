90b072ffbfc3bba8859466cd71bf3073b5d382fa
Moonshot Design Company <moonshotdesignco@gmail.com>
2018-03-31 19:06:35 -0400

    update

--- menu.js
+++ menu.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
 
-  $(".menu-trigger").click(function() {
-    if ($("#content-wrapper").css("left") == "0px") {
-      $("#content-wrapper").css({"left": "240px", "overflow": "hidden"});
-      $(".menu-trigger").addClass("open");
-    } else if ($("#content-wrapper").css("left") == "240px") {
-      $("#content-wrapper").css({"left": "0px", "overflow": "auto"});
-      $(".menu-trigger").removeClass("open");
+  $(".menu-button").click(function() {
+    if ($(".body-container").css("left") == "0px") {
+      $(".menu-button").addClass("open");
+      $(".body-container").css("position", "fixed");
+      $(".menu-container").css("right", "0px");
+    } else if ($(".body-container").css("left") == "-200px") {
+      $(".menu-button").removeClass("open");
+      $(".body-container").css("position", "relative");
+      $(".menu-container").css("right", "-200px");
     }
   });
 
-  $("#content-wrapper").click(function() {
-    if ($("#content-wrapper").css("left") == "240px") {
-      $("#content-wrapper").css({"left": "0px", "overflow": "auto"});
-      $(".menu-trigger").removeClass("open");
+  $("html, #close-button").click(function() {
+    if ($(".menu-container").css("right") == "0px") {
+      $(".menu-button").removeClass("open");
+      $(".body-container").css("position", "relative");
+      $(".menu-container").css("right", "-200px");
     }
+  });
+  
+  $(".menu-button, .menu-container").click(function(event){
+      event.stopPropagation();
   });
 
 });
