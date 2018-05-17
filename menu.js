b65ac0b3354592d035851861e105ed40d559ec00
will <moonshotdesignco@gmail.com>
2018-05-17 16:54:03 -0400

    menu fix

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
+  $(window, document, "html", "body").click(function() {
+    $(".menu").slideUp();
+  });
+
   $(".menu-trigger button").click(function() {
     $(".menu").slideToggle();
   });
 
   $(".menu-trigger button, .menu").click(function(e) {
     e.stopPropagation();
-  });
-  
-  $(window, document, "html", "body").click(function() {
-    $(".menu").slideUp();
   });
 
 });
