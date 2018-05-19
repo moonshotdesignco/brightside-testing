c6e76f84539341d6946fc7ac51eefc1b39323987
will <moonshotdesignco@gmail.com>
2018-05-19 17:20:36 -0400

    menu fix

--- menu.js
+++ menu.js
@@ -3,10 +3,8 @@ $(document).ready(function() {
   $(".menu-trigger").click(function() {
     if ($(".menu").css("display") == "none") {
       $(".menu-trigger img").addClass("open");
-      $(".block-section").addClass("menu-open");
     } else {
       $(".menu-trigger img").removeClass("open");
-      $(".block-section").removeClass("menu-open");
     }
     $(".menu").slideToggle(400);
   });
