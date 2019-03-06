c7b82124e4109d5efafd22d1ebfa423a20e539a5
will <moonshotdesignco@gmail.com>
2019-03-06 02:30:48 -0500

    improvements

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
   $("html").click(function() {
-    $("#nav").fadeOut(200, function() {
-      $("#trigger").removeClass("active");
-    });
+    $("#nav").hide(0);
+    $("#trigger").removeClass("active");
   });
   $("#trigger").click(function() {
     $(this).toggleClass("active");
-    $("#nav").fadeToggle(200);
+    $("#nav").toggle(0);
   });
   $("#trigger, #nav").click(function(e) {
     e.stopPropagation();
