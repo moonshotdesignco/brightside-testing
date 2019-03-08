d8a1af93d0ffe5bd002d1c0287681cf1a9d4f0d8
will <moonshotdesignco@gmail.com>
2019-03-07 21:18:25 -0500

    menu update

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 $(function(){
+  $("#trigger").click(function() {
+    $("html, body").toggleClass("freeze");
+    $(".container#header").toggleClass("open");
+    $(".container#content, .container#footer").fadeToggle(0);
+    $("#trigger").find(".fa-bars, .fa-times").toggleClass("fa-times fa-bars");
+    $("#nav").toggle(0);
+  });
   $("html, body").click(function() {
     $("#nav").hide(0);
-    $("#trigger").removeClass("active");
-  });
-  $("#trigger").click(function() {
-    $(this).toggleClass("active");
-    $("#nav").toggle(0);
+    $("#trigger").find(".fa-times").toggleClass("fa-bars");
+    $(".container#content, .container#footer").fadeIn(0);
+    $(".container#header").removeClass("open");
+    $("html, body").removeClass("freeze");
   });
   $("#trigger, #nav").click(function(e) {
     e.stopPropagation();
