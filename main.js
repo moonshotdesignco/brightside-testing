d973acf615a313897b1664f39465ee2d40d327bd
will <moonshotdesignco@gmail.com>
2019-03-08 18:47:34 -0500

    ux improvements

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 $(function(){
-  $("#trigger").click(function() {
+  $("#trigger > a").click(function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: $(".container#nav").offset().top
+    }, 700);
     $("body").addClass("freeze");
-    $("#nav").slideDown(700);
+    $(".container#header a, .container#content a, .container#footer a").attr("tabindex", "-1");
+    $(".container#nav").animate({
+      "min-height": "100%"
+    }, 700, function() {
+      $(".container#nav ul").css("overflow", "auto");
+      $(".container#nav ul li a").removeAttr("tabindex");
+    });
   });
-  $("#close").click(function() {
-    $("#nav").slideUp(700);
-    $("body").removeClass("freeze");
+  $("#close > a").click(function(e) {
+    e.preventDefault();
+    $(".container#nav ul").animate({
+      scrollTop: $("body").offset().top
+    }, 700);
+    $(".container#nav ul").css("overflow", "hidden");
+    $(".container#nav ul li a").attr("tabindex", "-1");
+    $(".container#nav").animate({
+      "min-height": "0%"
+    }, 700, function() {
+      $(".container#header a, .container#content a, .container#footer a").removeAttr("tabindex");
+      $("body").removeClass("freeze");
+    });
   });
 });
